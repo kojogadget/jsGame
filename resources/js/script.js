78efd6a34373d4964cf72e7720e69167eba80486
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-08-21 20:08:00 +0200

    Adding condition to script to only add name once

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,7 +5,11 @@ const welcomeMessage = document.querySelector(".message__line--2");
 const welcomeMessageText = document.querySelector(".message__text--2");
 
 command.addEventListener("keydown", function (e) {
-  if (e.key === "Enter" && command.value !== "") {
+  if (
+    e.key === "Enter" &&
+    command.value !== "" &&
+    Object.keys(player).length === 0
+  ) {
     player.name = command.value;
 
     welcomeMessageText.textContent = `Welcome, ${player.name}! What program do you want to try?`;
