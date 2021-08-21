ee8e8d4b26be6fafbc9509b58705d683ade6fb31
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-08-21 19:58:16 +0200

    Adding the welcome message

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+const player = {};
+
+const command = document.getElementById("command");
+const welcomeMessage = document.querySelector(".message__line--2");
+const welcomeMessageText = document.querySelector(".message__text--2");
+
+command.addEventListener("keydown", function (e) {
+  if (e.key === "Enter" && command.value !== "") {
+    player.name = command.value;
+
+    welcomeMessageText.textContent = `Welcome, ${player.name}! What program do you want to try?`;
+    welcomeMessage.classList.remove("hidden");
+  }
+});
