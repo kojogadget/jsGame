10372b161d3eeb302177c934ecf29ca9b1b93c7d
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-08-21 21:03:07 +0200

    Adding a submit button

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,7 @@
 const player = {};
 
 const command = document.getElementById("command");
+const btn = document.querySelector(".respons__btn");
 const welcomeMessage = document.querySelector(".message__line--2");
 const welcomeMessageText = document.querySelector(".message__text--2");
 
@@ -10,6 +11,15 @@ command.addEventListener("keydown", function (e) {
     command.value !== "" &&
     Object.keys(player).length === 0
   ) {
+    player.name = command.value;
+
+    welcomeMessageText.textContent = `Welcome, ${player.name}! What program do you want to try?`;
+    welcomeMessage.classList.remove("hidden");
+  }
+});
+
+btn.addEventListener("click", function () {
+  if (command.value !== "" && Object.keys(player).length === 0) {
     player.name = command.value;
 
     welcomeMessageText.textContent = `Welcome, ${player.name}! What program do you want to try?`;
