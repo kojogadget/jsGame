ac1e0307eaedb29bf9fce947603062f3f04cdb27
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-08-23 21:40:27 +0200

    Fixing typing error

--- resources/js/collatz.js
+++ resources/js/collatz.js
@@ -6,7 +6,7 @@ collatzBtn.addEventListener('click', function () {
   addHeading('Collatz');
   addHeading(`Math's simplest impossible problem!`);
   addLineMore(
-    `If you take a number through this equation the output will always become 1, what ever the number. 
+    `If you take a number through this equation the output will always become 1, whatever the number. 
     The equation will take a even number en divide it by 2, and a odd number will be multiplied by 3 and be added a 1. 
     The result will go through the same process and finally give us 1 as the final result.`
   );
