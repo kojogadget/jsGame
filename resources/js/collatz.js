2728132e5eff680491291758ebd2c78bf9acd5e5
Kjartan Gundersen <kjartangundersen.kg@gmail.com>
2021-08-25 00:42:27 +0200

    changing input in collatz to number pt 2

--- resources/js/collatz.js
+++ resources/js/collatz.js
@@ -6,7 +6,7 @@ collatzBtn.addEventListener('click', function () {
   addHeading('Collatz');
   addHeading(`Math's simplest impossible problem!`);
   addLineMore(
-    `If you take a number through this equation the output will always become 1, whatever the number. 
+    `If you take a number through this equation the output will always become 1, whatever positive number. 
     The equation will take a even number en divide it by 2, and a odd number will be multiplied by 3 and be added a 1. 
     The result will go through the same process and finally give us 1 as the final result.`
   );
@@ -17,20 +17,24 @@ collatzBtn.addEventListener('click', function () {
 });
 
 const collatz = function () {
-  collatzNumber = Number(responsInput.value);
-  collatzEquation += String(collatzNumber);
+  if (Number(responsInput.value) > 0) {
+    collatzNumber = Number(responsInput.value);
+    collatzEquation += String(collatzNumber);
 
-  while (collatzNumber !== 1) {
-    if (collatzNumber % 2 === 0) {
-      collatzNumber = collatzNumber / 2;
-      collatzEquation += ' -> ';
-      collatzEquation += String(collatzNumber);
-    } else {
-      collatzNumber = collatzNumber * 3 + 1;
-      collatzEquation += ' -> ';
-      collatzEquation += String(collatzNumber);
+    while (collatzNumber !== 1) {
+      if (collatzNumber % 2 === 0) {
+        collatzNumber = collatzNumber / 2;
+        collatzEquation += ' -> ';
+        collatzEquation += String(collatzNumber);
+      } else {
+        collatzNumber = collatzNumber * 3 + 1;
+        collatzEquation += ' -> ';
+        collatzEquation += String(collatzNumber);
+      }
     }
+    addLineCollatz(collatzEquation);
+    collatzEquation = '';
+  } else {
+    addLine('The number must be positive..');
   }
-  addLineCollatz(collatzEquation);
-  collatzEquation = '';
 };
